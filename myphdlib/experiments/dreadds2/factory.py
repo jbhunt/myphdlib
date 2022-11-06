8b18a65d9bd32be7e4a7c9f6fa38233e98520381
jbhunt <hunt.brian.joshua@gmail.com>
2022-11-05 19:50:08 -0600

    Implemented iterator protocol for the dreads2 experiment

--- myphdlib/experiments/dreadds2/factory.py
+++ myphdlib/experiments/dreadds2/factory.py
@@ -8,7 +8,60 @@ class Session():
         """
         """
 
+        # Folders
+        self.sessionFolderPath = pl.Path(sessionFolder)
+        self.videosFolderPath = self.sessionFolderPath.joinpath('videos')
+
+        # Files
+        self.inputFilePath = self.sessionFolderPath.joinpath('input.txt')
+        self.outputFilePath = self.sessionFolderPath.joinpath('output.pickle')
+        self.driftingGratingMetadataFilePath = self.videosFolderPath.joinpath('driftingGratingMetadata.txt')
+
+        # Determine the animal, date, and treatment
+        self.notesFilePath = self.sessionFolderPath.joinpath('notes.txt')
+        self.animal, self.date, self.treatment = None, None, None
+        if self.notesFilePath.exists():
+            with open(self.notesFilePath, 'r') as stream:
+                lines = stream.readlines()
+            for line in lines:
+                for attribute in ('animal', 'date', 'experiment'):
+                    if bool(re.search(f'{attribute}*', line.lower())) and line.startswith('-') == False:
+                        value = line.lower().split(': ')[-1].rstrip('\n')
+                        setattr(self, attribute, value)
+
         return
+
+    def load(self, name):
+        """
+        """
+
+        if self.outputFilePath.exists() == False:
+            raise Exception('Could not locate output file')
+
+        with open(self.outputFilePath, 'rb') as stream:
+            dataContainer = pickle.load(stream)
+
+        if name not in dataContainer.keys():
+            raise Exception(f'Invalid data key: {name}')
+        else:
+            return dataContainer[name]
+
+    @property
+    def fps(self):
+        """
+        Video acquisition framerate
+        """
+
+        framerate = None
+        result = list(self.videosFolderPath.glob('*metadata.yaml'))
+        if result:
+            with open(result.pop(), 'r') as stream:
+                acquisitionMetadata = yaml.safe_load(stream)
+            for cameraAlias in ('cam1', 'cam2'):
+                if acquisitionMetadata[cameraAlias]['ismaster']:
+                    framerate = acquisitionMetadata[cameraAlias]['framerate']
+
+        return framerate
 
 def SessionFactory(SessionFactoryBase):
     """
@@ -32,10 +85,30 @@ def SessionFactory(SessionFactoryBase):
         """
         """
 
-        return
+        sessionLocated = False
+        for session in self:
+            if session.animal == animal and session.date == date:
+                sessionLocated = True
+                break
+        
+        if sessionLocated:
+            return session
+        else:
+            raise Exception('Could not locate session')
 
+    # Iterator protocol definition
     def __iter__(self):
-        return
+        self.sessionFolders = list()
+        for date in self.rootFolderPath.iterdir():
+            for animal in date.iterdir():
+                self.sessionFolders.append(str(animal))
+        self._listIndex = 0
+        return self
 
     def __next__(self):
-        return
+        if self._listIndex < len(self.sessionFolders):
+            sessionFolder = self.sessionFolders[self._listIndex]
+            self._listIndex += 1
+            return Session(sessionFolder)
+        else:
+            raise StopIteration
