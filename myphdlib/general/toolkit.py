a85053eb081292c3c8cc9c254401b21b73a00443
jbhunt <hunt.brian.joshua@gmail.com>
2022-12-06 12:00:01 -0800

    Made an inner for loop in the psth function conditional to save time computing the psth

--- myphdlib/general/toolkit.py
+++ myphdlib/general/toolkit.py
@@ -121,8 +121,9 @@ def psth(target_events, relative_events, binsize=0.01, window=(-0.5, 1), edges=N
         for iev, target_event in enumerate(target_events):
             referenced = relative_events - target_event
             within_window_mask = np.logical_and(referenced > start, referenced <= stop)
-            for relative_time in referenced[within_window_mask]:
-                relative_times.append(relative_time)
+            if return_relative_times:
+                for relative_time in referenced[within_window_mask]:
+                    relative_times.append(relative_time)
             counts, edges_ = np.histogram(referenced[within_window_mask], bins=edges)
             M.append(counts)
 
