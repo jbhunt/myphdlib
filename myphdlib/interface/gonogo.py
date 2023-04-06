0d957b9e3357bd3fe2d34783b9616d420dc646b0
annabuteau88 <anna.buteau@cuanschutz.edu>
2023-04-06 09:15:44 -0600

    still messing around

--- myphdlib/interface/gonogo.py
+++ myphdlib/interface/gonogo.py
@@ -677,7 +677,7 @@ class GonogoSession(SessionBase):
         contrastValues = self.extractContrastValues()
         totalSaccades = self.extractSaccadeTimestamps()
         zipped3, perisaccadicProbeBool = self.createZippedList(totalSaccades)
-        zipped3 = zipped3[0:100]
+        zipped3 = zipped3[0:80]
         zipTrue, zipFalse, listCT, listPT, listCF, listPF = self.createPeriAndExtraSaccadicLists(perisaccadicProbeBool, contrastValues)
         dictionaryTrue = self.createPerisaccadicDictionary(listCT, listPT)
         dictionaryFalse = self.createExtrasaccadicDictionary(listCF, listPF)
