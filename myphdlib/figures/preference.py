5c5fe627507046e888eabcd963c47906dfbea7b1
jbhunt <hunt.brian.joshua@gmail.com>
2024-05-09 11:21:07 -0600

    Working on direction selectivity analysis

--- myphdlib/figures/preference.py
+++ myphdlib/figures/preference.py
@@ -52,6 +52,12 @@ class DirectionSectivityAnalysis(
             'nasal': None,
             'temporal': None,
         }
+        self.mi = {
+            'real': None,
+        }
+        self.p = {
+            'real': None,
+        }
 
         return
 
@@ -72,8 +78,13 @@ class DirectionSectivityAnalysis(
             'preference/dsi/bar': (self.dsi, 'bar'),
             'preference/dsi/probe': (self.dsi, 'probe'),
             'preference/dsi/saccade': (self.dsi, 'saccade'),
+            'preference/pd/bar': (self.pd, 'bar'),
+            'preference/pd/probe': (self.pd, 'probe'),
+            'preference/pd/saccade': (self.pd, 'saccade'),
             'modulation/templates/nasal': (self.templates, 'nasal'),
             'modulation/templates/temporal': (self.templates, 'temporal'),
+            'modulation/mi': (self.mi, 'real'),
+            'bootstrap/p': (self.p, 'real'),
         }
         with h5py.File(self.hdf, 'r') as stream:
             for path, (attr, key) in datasets.items():
@@ -110,6 +121,9 @@ class DirectionSectivityAnalysis(
             'preference/dsi/bar': (self.dsi['bar'], True),
             'preference/dsi/probe': (self.dsi['probe'], True),
             'preference/dsi/saccade': (self.dsi['saccade'], True),
+            'preference/pd/bar': (self.pd['bar'], True),
+            'preference/pd/probe': (self.pd['probe'], True),
+            'preference/pd/saccade': (self.pd['saccade'], True),
         }
 
         #
@@ -166,40 +180,31 @@ class DirectionSectivityAnalysis(
 
     def measureDirectionSelectivityForProbes(
         self,
+        responseWindow=(0, 0.5),
         ):
         """
         """
 
+        binIndices = np.where(np.logical_and(
+            self.tProbe >= responseWindow[0],
+            self.tProbe <= responseWindow[1]
+        ))[0]
         self.dsi['probe'] = np.full(len(self.ukeys), np.nan)
         self.pd['probe'] = np.full(len(self.ukeys), np.nan)
         for iUnit in range(len(self.ukeys)):
 
             #
-            a1 = self.model['params1'][iUnit, 0]
-            params = self.model['params1'][iUnit, :]
-            params = np.delete(params, np.isnan(params))
-            abc, d = params[:-1], params[-1]
-            A1, B1, C1 = np.split(abc, 3)
+            a1 = np.abs(self.peths['preferred'][iUnit, binIndices]).max()
+            a2 = np.abs(self.peths['null'][iUnit, binIndices]).max()
+            pd = self.features['d'][iUnit]
 
             #
-            params = self.model['params2'][iUnit, :]
-            params = np.delete(params, np.isnan(params))
-            abc, d = params[:-1], params[-1]
-            A2, B2, C2 = np.split(abc, 3)
-            iComp = np.argmin(B2 - B1[0])
-            a2 = A2[iComp]
-
-            #
-            if (a1 < 0 and a2 > 0) or (a1 > 0 and a2 < 0):
-                a2 = 0
-
-            #
-            t1 = np.deg2rad(180 if self.features['d'][iUnit] == -1 else 0)
-            t2 = np.deg2rad(0 if t1 == 180 else 180)
-            vectors = np.array([
-                [a1, t1],
-                [a2, t2]
-            ])
+            vectors = np.full([2, 2], np.nan)
+            vectors[:, 0] = np.array([a1, a2]).T
+            vectors[:, 1] = np.array([
+                np.pi if pd == -1 else 0,
+                0 if pd == -1 else np.pi
+            ]).T
 
             # Compute the coordinates of the polar plot vertices
             vertices = np.vstack([
@@ -209,9 +214,9 @@ class DirectionSectivityAnalysis(
 
             # Compute direction selectivity index
             a, b = vertices.sum(0) / vectors[:, 0].sum()
-            dsi = np.sqrt(np.power(a - 0, 2) + np.power(b - 0, 2))
+            dsi = np.sqrt(np.power(a, 2) + np.power(b, 2))
             self.dsi['probe'][iUnit] = dsi
-            self.pd['probe'][iUnit] = t1
+            self.pd['probe'][iUnit] = vectors[0, 1]
 
         return
 
@@ -231,6 +236,9 @@ class DirectionSectivityAnalysis(
         for iUnit in range(len(self.ukeys)):
 
             #
+            self.ukey = self.ukeys[iUnit]
+
+            #
             a1 = np.max(np.abs(self.templates['nasal'][iUnit, binIndices]))
             a2 = np.max(np.abs(self.templates['temporal'][iUnit, binIndices]))
             pd = 'nasal' if a1 > a2 else 'temporal'
@@ -246,8 +254,6 @@ class DirectionSectivityAnalysis(
                     vectors[:, 0] = np.array([a2, a1]).T
                     vectors[:, 1] = np.array([np.deg2rad(0), np.deg2rad(180)]).T
                     self.pd['saccade'][iUnit] = np.deg2rad(0)
-            elif self.session.eye == 'right':
-                raise Exception('Right eye sessions not implemented yet')
 
             # Compute the coordinates of the polar plot vertices
             vertices = np.vstack([
@@ -266,19 +272,70 @@ class DirectionSectivityAnalysis(
         self,
         ):
         """
-        Extract DSI for the moving bars stimulus
+        Compute DSI for the moving bars stimulus
         """
 
         self.dsi['bar'] = np.full(len(self.ukeys), np.nan)
         self.pd['bar'] = np.full(len(self.ukeys), np.nan)
         date = None
-        for ukey in self.ukeys:
-            self.ukey = ukey
-            if date is None or ukey[0] != date:
-                dsi = self.session.load('metrics/dsi')
-                date = self.session.date
-            iUnit = self._indexUnitKey(ukey)
-            self.dsi['bar'][iUnit] = dsi[self.unit.index]
+        for session in self.sessions:
+
+            #
+            self._session = session
+
+            # 
+            movingBarOrientations = self.session.load('stimuli/mb/orientation')
+            barOnsetTimestamps = self.session.load('stimuli/mb/onset/timestamps')
+            barOffsetTimestamps = self.session.load('stimuli/mb/offset/timestamps')
+            movingBarTimestamps = np.hstack([
+                barOnsetTimestamps.reshape(-1, 1),
+                barOffsetTimestamps.reshape(-1, 1)
+            ])
+            uniqueOrientations = np.unique(movingBarOrientations)
+            uniqueOrientations.sort()
+
+            #
+            for ukey in self.ukeys:
+
+                #
+                if ukey[0] != str(session.date):
+                    continue
+                self.ukey = ukey
+
+                #
+                vectors = np.full([uniqueOrientations.size, 2], np.nan)
+                for rowIndex, orientation in enumerate(uniqueOrientations):
+
+                    #
+                    trialIndices = np.where(movingBarOrientations == orientation)[0]
+                    amplitudes = list()
+                    for trialIndex in trialIndices:
+                        t1, t2 = movingBarTimestamps[trialIndex, :]
+                        dt = t2 - t1
+                        t, M = psth2(
+                            np.array([t1]),
+                            self.unit.timestamps,
+                            window=(0, dt),
+                            binsize=None
+                        )
+                        fr = M.item() / dt
+                        amplitudes.append(fr)
+
+                    #
+                    vectors[rowIndex, 0] = np.mean(amplitudes)
+                    vectors[rowIndex, 1] = np.deg2rad(orientation)
+
+                # Compute the coordinates of the polar plot vertices
+                vertices = np.vstack([
+                    vectors[:, 0] * np.cos(vectors[:, 1]),
+                    vectors[:, 0] * np.sin(vectors[:, 1])
+                ]).T
+
+                # Compute direction selectivity index
+                a, b = vertices.sum(0) / vectors[:, 0].sum()
+                self.dsi['bar'][self.iUnit] = np.sqrt(np.power(a, 2) + np.power(b, 2))
+                self.pd['bar'][self.iUnit] = np.arctan2(b, a) % (2 * np.pi)
+
 
         return
 
@@ -298,20 +355,92 @@ class DirectionSectivityAnalysis(
 
         return
 
-    def plotDirectionSelectivityIndices(
+    def plotModulationByDirectionSelectivity(
         self,
+        threshold=0.3,
+        nBins=50,
+        figsize=(4, 5),
         ):
         """
         """
 
-        fig, ax = plt.subplots()
-        ax.scatter(
-            self.dsi['probe'],
-            self.dsi['saccade'],
-            marker='.',
-            s=10,
-            color='k',
-            alpha=0.7
+        #
+        fig, grid = plt.subplots(nrows=3, sharex=True)
+        mi = self.mi['real'][:, 5, 0] / self.model['params1'][:, 0]
+        modulated = self.p['real'][:, 0] < 0.05
+        for i, ev in enumerate(['bar', 'probe', 'saccade']):
+            samples = (
+                mi[np.vstack([self.filter, modulated, self.dsi[ev] >= threshold]).all(0)],
+                mi[np.vstack([self.filter, modulated, self.dsi[ev] <  threshold]).all(0)],
+            )
+            counts, edges, patches = grid[i].hist(
+                samples,
+                range=(-3, 3),
+                bins=nBins,
+                histtype='barstacked'
+            )
+            for patch in patches[0]:
+                patch.set_facecolor('k')
+                patch.set_edgecolor('k')
+            for patch in patches[1]:
+                patch.set_facecolor('w')
+                patch.set_edgecolor('k')
+
+        #
+        titles = (
+            'Moving bars',
+            'Probe',
+            'Saccade'
         )
+        for i, ax in enumerate(grid):
+            ax.set_ylabel('N units')
+            ax.set_title(titles[i], fontsize=10)
+        grid[-1].set_xlabel('Modualtion index (MI)')
+        grid[0].legend([r'$DSI\geq 0.3$', r'$DSI<0.3$'])
+        fig.set_figwidth(figsize[0])
+        fig.set_figheight(figsize[1])
+        fig.tight_layout()
+
+        return fig, grid
+
+    def scatterDirectionSelectivityIndices(
+        self,
+        events=('probe', 'saccade'),
+        figsize=(4, 4),
+        ):
+        """
+        """
+
+        ev1, ev2 = events
+        fig, ax = plt.subplots()
+        data = {
+            events[0]: list(),
+            events[1]: list()
+        }
+        for iUnit in range(len(self.ukeys)):
+            if self.filter[iUnit] == False:
+                continue
+            for ev in events:
+                xRad = self.pd[ev][iUnit]
+                xSigned = -1 if xRad > 1.5 else +1
+                xNorm = xSigned * self.dsi[ev][iUnit]
+                data[ev].append(xNorm)
+
+        ax.scatter(
+            *data.values(),
+            marker='.',
+            s=7,
+            color='k',
+            alpha=0.5,
+            clip_on=False
+        )
+        ax.set_xlim([-1, 1])
+        ax.set_ylim([-1, 1])
+        ax.set_aspect('equal')
+        ax.set_xlabel(r'$DSI_{Probe}\cdot PD_{Probe}$')
+        ax.set_ylabel(r'$DSI_{Saccade}\cdot PD_{Saccade}$')
+        fig.set_figwidth(figsize[0])
+        fig.set_figheight(figsize[1])
+        fig.tight_layout()
 
         return fig, ax
