cf1086ae4a9cfb3ef9a40000883eb2d4ad099027
jbhunt <hunt.brian.joshua@gmail.com>
2024-11-25 09:04:47 -0700

    Added an analysis for descripting saccade related activity

---
+++ myphdlib/figures/premotor.py
@@ -0,0 +1,13 @@
+from myphdlib.figures.analysis import AnalysisBase
+
+class SaccadeResponseAnalysis(AnalysisBase):
+    """
+    """
+
+    def __init__(self, **kwargs):
+        """
+        """
+
+        super().__init__(event='saccade', **kwargs)
+
+        return
